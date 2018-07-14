99414ca0badcbccbf0dc2a5fe562e8566432ef6b
JoUnicorn <jogithub@gmail.com>
2018-07-14 21:15:16 +0800

    formating

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,12 @@ import './Header.css';
 
 const Header = ({onClick }) => (
   <div className="header w3-bar w3-top w3-white w3-xlarge">
-    <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={() => onClick()}><i className="fa fa-bars"></i>  Menu</button>
-    <span className="w3-bar-item w3-left w3-hide-medium w3-hide-small">Data team</span>
-    <button className="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-left w3-hover-none w3-hover-text-light-grey">Project</button>
-    <button className="w3-bar-item w3-button w3-hide-medium w3-hide-small w3-left w3-hover-none w3-hover-text-light-grey">Add component</button>
+    <div className="w3-content largeur">
+      <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={() => onClick()}><i className="fa fa-bars"></i>  Menu</button>
+      <span className="w3-bar-item w3-left w3-hide-medium w3-hide-small">Data team</span>
+      <button className="w3-large w3-bar-item w3-button w3-hide-medium w3-hide-small w3-left w3-hover-none w3-hover-text-light-grey">Project</button>
+      <button className="w3-large w3-bar-item w3-button w3-hide-medium w3-hide-small w3-left w3-hover-none w3-hover-text-light-grey">Add component</button>
+    </div>
   </div>
 )
 
