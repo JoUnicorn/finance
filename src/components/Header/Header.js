424d75427262e726ec9517dc39062aa52ac35069
JoUnicorn <jogithub@gmail.com>
2018-07-15 12:56:16 +0800

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = ({onClick }) => (
   <div className="header w3-bar w3-top w3-white w3-xlarge">
-    <div className="w3-content largeur">
+    <div className="largeur">
       <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={() => onClick()}><i className="fa fa-bars"></i>  Menu</button>
       <span className="w3-bar-item w3-left w3-hide-medium w3-hide-small">Data team</span>
       <button className="w3-large w3-bar-item w3-button w3-hide-medium w3-hide-small w3-left w3-hover-none w3-hover-text-light-grey">Project</button>
